9ec52c14a95c0df8a3c928b365ce890719b4e6bb
@prasai029096Pp <gamernp1234@gmail.com>
2026-06-27 06:01:57 +0545

    Fix onResume access modifier in MainActivity

--- android/app/src/main/java/app/lovable/jackpotjungle/MainActivity.java
+++ android/app/src/main/java/app/lovable/jackpotjungle/MainActivity.java
@@ -97,7 +97,7 @@ public class MainActivity extends BridgeActivity {
     }
 
     @Override
-    protected void onResume() {
+    public void onResume() {
         super.onResume();
         setupLockscreenFlags();
     }
