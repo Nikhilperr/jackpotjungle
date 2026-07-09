1b34d0cfb6f0cc094b144d596e5f6b124af48a4a
@prasai029096Pp <gamernp1234@gmail.com>
2026-07-09 21:45:01 +0545

    Fix: correct TYPE_WIRED_HEADPHONE spelling to TYPE_WIRED_HEADPHONES inside MainActivity.java

--- android/app/src/main/java/app/lovable/jackpotjungle/MainActivity.java
+++ android/app/src/main/java/app/lovable/jackpotjungle/MainActivity.java
@@ -161,7 +161,7 @@ public class MainActivity extends BridgeActivity {
                                             int type = device.getType();
                                             if (type == android.media.AudioDeviceInfo.TYPE_BLUETOOTH_A2DP ||
                                                 type == android.media.AudioDeviceInfo.TYPE_BLUETOOTH_SCO ||
-                                                type == android.media.AudioDeviceInfo.TYPE_WIRED_HEADPHONE ||
+                                                type == android.media.AudioDeviceInfo.TYPE_WIRED_HEADPHONES ||
                                                 type == android.media.AudioDeviceInfo.TYPE_WIRED_HEADSET ||
                                                 type == android.media.AudioDeviceInfo.TYPE_USB_HEADSET) {
                                                 hasHeadset = true;
